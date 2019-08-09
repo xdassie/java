aea67dbad10aea8d681f35c542a3ae68b8436e9f
xdassie <xdassie@gmail.com>
2019-08-09 17:54:57 +0200

    adding spark dependencies and imports

--- src/main/java/NoPackageTest.java
+++ src/main/java/NoPackageTest.java
@@ -1,3 +1,7 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 import org.testng.annotations.Test;
 public class NoPackageTest {
 @Test
